4733ac62b7b55c076fa87dc345432c63242c2a8e
Nahuel1077 <nahuh08@gmail.com>
2026-09-15 12:41:05 -0300

    Changed contact number

--- app/components/footer.js
+++ app/components/footer.js
@@ -22,7 +22,7 @@ export default function Footer() {
             <div className="flex flex-col items-center gap-4">
                 <h2 className="text-2xl font-bold">Contact</h2>
                 <ul className="flex flex-col items-center gap-2">
-                    <li><Link className="hover:opacity-80" href="https://api.whatsapp.com/send/?phone=5491127194458&text=Hola,%20te%20hablo%20desde%20el%20sitio%20web%20de%20MediXperts&type=phone_number&app_absent=0">Support</Link></li>
+                    <li><Link className="hover:opacity-80" href="https://api.whatsapp.com/send/?phone=5491127194665&text=Hola,%20te%20hablo%20desde%20el%20sitio%20web%20de%20MediXperts&type=phone_number&app_absent=0">Support</Link></li>
                     <li>Blog</li>
                     <li><Link
                             href="https://drive.google.com/uc?export=download&id=1APFfbOWbMRfEC-pq2vRU4nQO7Q14Uj-h"
@@ -34,7 +34,7 @@ export default function Footer() {
                         </Link></li>
                 </ul>
             </div>
-                    <Link className="fixed top-4/5 left-9/12 lg:left-11/12 z-50 bg-pink-500 invert hover:bg-pink-400 rounded-full w-[80px] h-[80px] flex flex-col items-center justify-center" href="https://api.whatsapp.com/send/?phone=5491127194458&text=Hola,%20te%20hablo%20desde%20el%20sitio%20web%20de%20MediXperts&type=phone_number&app_absent=0"><Image src="/img/whatsapp-brands.svg" width={40} height={40} alt="logo whatsapp"/></Link>
+                    <Link className="fixed top-4/5 left-9/12 lg:left-11/12 z-50 bg-pink-500 invert hover:bg-pink-400 rounded-full w-[80px] h-[80px] flex flex-col items-center justify-center" href="https://api.whatsapp.com/send/?phone=5491127194665&text=Hola,%20te%20hablo%20desde%20el%20sitio%20web%20de%20MediXperts&type=phone_number&app_absent=0"><Image src="/img/whatsapp-brands.svg" width={40} height={40} alt="logo whatsapp"/></Link>
             
         </footer>
     )
